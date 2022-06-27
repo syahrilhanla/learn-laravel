a123336d47215a7c9bec4c4cb814892faf34f82a
syahrilhanla <syahrilhanla5@gmail.com>
2022-06-27 11:21:38 +0800

    added migration for create listing

--- database/migrations/2022_06_27_031528_create_listings_table.php
+++ database/migrations/2022_06_27_031528_create_listings_table.php
@@ -16,6 +16,12 @@ return new class extends Migration
         Schema::create('listings', function (Blueprint $table) {
             $table->id();
             $table->string('title');
+            $table->string('tags');
+            $table->string('company');
+            $table->string('location');
+            $table->string('email');
+            $table->string('website');
+            $table->longText('description');
             $table->timestamps();
         });
     }

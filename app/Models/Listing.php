98238e195d0d00a13dbf8a9cb318453aed9b9679
syahrilhanla <syahrilhanla5@gmail.com>
2022-06-29 11:46:58 +0800

    made listing model and factory with artisan

--- app/Models/Listing.php
+++ app/Models/Listing.php
@@ -1,37 +1,11 @@
 <?php
-  namespace App\Models;
 
-use phpDocumentor\Reflection\Types\Self_;
+namespace App\Models;
 
-  class Listing {
-    public static function all() {
-      return  [
-            [
-                'id' => "1",
-                "title" => "list one",
-                'description' => "this is a tryout for laravel learning"
-            ],
-             [
-                'id' => "2",
-                "title" => "list two",
-                'description' => "this is a tryout for laravel learning"
-             ],
-             [
-                'id' => "3",
-                "title" => "list three",
-                'description' => "this is a tryout for laravel learning"
-            ]
-             ];
-    }
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-    public static function find($id) {
-      $results = self::all();
-
-      foreach ($results as $result)  {
-        if ($result['id'] === $id) {
-          return $result;
-        }
-      }
-    }
-  }
-?>
+class Listing extends Model
+{
+    use HasFactory;
+}

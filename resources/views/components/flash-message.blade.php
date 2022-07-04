ac0e1a2b05330b532d46f5587be7824f2c1b0d6c
syahrilhanla <syahrilhanla5@gmail.com>
2022-07-04 14:27:04 +0800

    added alpine.js to remove flash message in 3 seconds

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,7 @@
 @if (session()->has('message'))
-    <div class="fixed top-0 left-[30%] px-36 py-3 bg-laravel transform-translate-x-1/2 text-white">
+    <div class="fixed top-0 left-[30%] px-36 py-3
+      bg-laravel transform-translate-x-1/2 text-white"
+        x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show='show'>
         <p>
             {{ session('message') }}
         </p>
